d293c93a779c96bca78f3d73cafcc83823794f81
Tostka <tostka@users.noreply.github.com>
2020-04-07 16:07:36 -0500

    Update c-winsallk.ps1
    4:07 PM 4/7/2020 added delimts to function .ps1

--- Public/c-winsallk.ps1
+++ Public/c-winsallk.ps1
@@ -1,1 +1,3 @@
-﻿function c-winsallk {. C:\usr\work\ps\scripts\close-WinsAll.ps1 -kill  }
+﻿#*------v Function c-winsallk v------
+function c-winsallk {. C:\usr\work\ps\scripts\close-WinsAll.ps1 -kill  }
+#*------^ END Function c-winsallk ^------
